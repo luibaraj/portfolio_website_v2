d032f1b75b578e3e63ceb9adb2555eeffef79503
luibaraj <barajasluis525@gmail.com>
2022-10-02 17:25:34 -0700

    Adding a third project

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,7 +6,7 @@ const Project = ({projectName, role, summary, tech, link}) => {
         <div className="Text">
             <p className="p1">{projectName + ' | role: '}<span>{role}</span></p>
 
-            <p className="p3"><span>Tech Stack: </span>{tech}&emsp;<a href={link} target="_blank" rel="noreferrer">Click for Demo</a></p>
+            <p className="p3"><span>Tech Stack: </span>{tech}&emsp;{link && <a href={link} target="_blank" rel="noreferrer">Click for Demo</a>}</p>
 
             <p className="p2"><span>Summary: </span>{summary}</p>
         </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,11 +4,11 @@ import {useState, useEffect} from 'react';
 
 const Projects = () => {
     const [slide, setSlide] = useState(0);
-    const [shows, setShows] = useState([false, false]);
+    const [shows, setShows] = useState([false, false, false]);
     
 
     useEffect(() => {
-        let newShows = [false, false];
+        let newShows = [false, false, false];
         newShows[slide] = true;
         setShows(newShows);
 
@@ -36,6 +36,7 @@ const Projects = () => {
             <div id="Projects" className="Proj-container">
                 {shows[0] && <Project className="mySlides"  key='1' projectName="Porfolio Website" role="front-end developer" summary="The features included in this project are an 'About me' section, a project section, skills, and my contact information. I implemented the use of github for version control, developed the site itself using React JS and styled it with css, and made my page responsive to ensure it is viewable on multiple devices before uploading it to the web using netlify." tech="HTML, CSS, and Javascript (React)" link="https://luisbarajas-web-portfolio.netlify.app/"/>}
                 {shows[1] && <Project className="mySlides"  key='2' projectName="Client for Reddit" role="front-end developer" summary="This project features a search bar used to search for a subreddit. If the searched subreddit does not exist or is private then an alert will appear. When the search is complete, links to posts of the searched subreddit will appear below the search bar." tech="HTML, CSS, and Javascript (React, fetch API)" link="https://client-for-reddit.netlify.app/"/>}
+                {shows[2] && <Project className="mySlides"  key='3' projectName="Weather App" role="front-end developer" summary="The features in this project allow you to search for the current weather of any city using the city's name. Features include the current temperture, highest and lowest recorded temp, humidity and cloudiness levels, and wind speed. This project was developed using React and was styled with css. Github was also used for version control." tech="HTML, CSS, and Javascript (React, fetch API)" link={false}/>}
             </div>
             <div className='buttons'>
                 <button className="w3-button w3-display-left" onClick={subOne}>&#10094;</button>
